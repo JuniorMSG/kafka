8226e332d613257ed0cee60474ec53259aa1a0de
JuniorMSG <milk6645@gmail.com>
2023-01-04 23:53:27 +0900

    kafka study - 1.4

--- kafka_study 2/src/main/java/org/example/controller/KafkaController.java
+++ kafka_study 2/src/main/java/org/example/controller/KafkaController.java
@@ -8,11 +8,13 @@ import org.apache.kafka.clients.producer.ProducerRecord;
 import org.example.model.TodoEntity;
 import org.example.model.TodoRequest;
 import org.example.model.TodoResponse;
+import org.example.services.EmailService;
 import org.example.services.TodoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
 import java.util.List;
 import java.util.Properties;
 import java.util.stream.Collectors;
@@ -27,13 +29,10 @@ public class KafkaController {
     @PostMapping("/producerFireForgot")
     public void create(@RequestBody TodoRequest request) {
 //        ProducerFireForgot
-        System.out.println("1234");
         Properties props = new Properties(); //Properties 오브젝트를 시작합니다.
         props.put("bootstrap.servers", "localhost:9092"); //브로커 리스트를 정의합니다.
-        props.put("key.serializer",
-                "org.apache.kafka.common.serialization.StringSerializer"); //메시지 키와 벨류에 문자열을 지정하므로 내장된 StringSerializer를 지정합니다.
-        props.put("value.serializer",
-                "org.apache.kafka.common.serialization.StringSerializer");
+        props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer"); //메시지 키와 벨류에 문자열을 지정하므로 내장된 StringSerializer를 지정합니다.
+        props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
         Producer<String, String> producer = new KafkaProducer<>(props); //Properties 오브젝트를 전달해 새 프로듀서를 생성합니다.
 
@@ -49,5 +48,12 @@ public class KafkaController {
             producer.close(); // 프로듀서 종료
         }
     }
+
+    @PostMapping("mail_test")
+    public void mail_test() throws MessagingException {
+        EmailService emailService = new EmailService();
+        emailService.sendEmail();
+    }
+
 
 }
---
+++ kafka_study 2/src/main/java/org/example/services/EmailService.java
@@ -0,0 +1,48 @@
+package org.example.services;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+@Service
+@AllArgsConstructor
+public class EmailService {
+    static Properties mailServerProperties;
+    static Session getMailSession;
+    static MimeMessage generateMailMessage;
+    public static void sendEmail() throws MessagingException {
+        // Step1
+        System.out.println("\n 1st ===> setup Mail Server Properties..");
+        mailServerProperties = System.getProperties();
+        mailServerProperties.put("mail.smtp.host", "smtp.naver.com");
+        mailServerProperties.put("mail.smtp.port", "465");
+        mailServerProperties.put("mail.smtp.auth", "true");
+        mailServerProperties.put("mail.smtp.starttls.enable", "true");
+        System.out.println("Mail Server Properties have been setup successfully..");
+
+        // Step2
+        System.out.println("\n\n 2nd ===> get Mail Session..");
+        getMailSession = Session.getDefaultInstance(mailServerProperties, null);
+        generateMailMessage = new MimeMessage(getMailSession);
+        generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress("milk6645@naver.com"));
+        generateMailMessage.addRecipient(Message.RecipientType.TO, new InternetAddress("milk6645@naver.com"));
+        generateMailMessage.setSubject("Greetings from Crunchify..");
+        String emailBody = "Test email by Crunchify.com JavaMail API example. " + "<br><br> Regards, <br>Crunchify Admin";
+        generateMailMessage.setContent(emailBody, "text/html");
+        System.out.println("Mail Session has been created successfully..");
+
+        // Step3
+        System.out.println("\n\n 3rd ===> Get Session and Send mail");
+        Transport transport = getMailSession.getTransport("smtp");
+
+        // Enter your correct gmail UserID and Password
+        // if you have 2FA enabled then provide App Specific Password
+        transport.connect("smtp.naver.com", "milk6645", "dkfltnsrltkfkd($");
+        transport.sendMessage(generateMailMessage, generateMailMessage.getAllRecipients());
+        transport.close();
+    }
+}
